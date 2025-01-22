559571df751b0cc77bb379532f1c3374f9d44537
cleverBe <cleverbernal123@gmail.com>
2025-01-22 15:24:56 -0400

    added pdf libraries

--- src/pages/WorkDistributions/Admin/components/WorkDistributionCharts.tsx
+++ src/pages/WorkDistributions/Admin/components/WorkDistributionCharts.tsx
@@ -19,6 +19,19 @@ interface CustomTooltipProps {
     name: string
     value: number
   }[]
+  total: number
+}
+
+const CustomTooltip = ({ active, payload, total }: CustomTooltipProps) => {
+  if (active && payload && payload.length) {
+    const percentage = ((payload[0].value / total) * 100).toFixed(2)
+    return (
+      <div className="rounded-md border bg-white p-2 shadow-md">
+        <p>{`${payload[0].name}: ${percentage}%`}</p>
+      </div>
+    )
+  }
+  return null
 }
 
 export const WorkDistributionCharts = ({ tasks }: { tasks: Task[] }) => {
@@ -54,71 +67,79 @@ export const WorkDistributionCharts = ({ tasks }: { tasks: Task[] }) => {
 
   const total = projectsGroups.reduce((acc, p) => acc + p.value, 0)
 
-  const CustomTooltip = ({ active, payload }: CustomTooltipProps) => {
-    if (active && payload && payload.length) {
-      const percentage = ((payload[0].value / total) * 100).toFixed(2)
-      return (
-        <div className="rounded-md border bg-white p-2 shadow-md">
-          <p>{`${payload[0].name}: ${percentage}%`}</p>
-        </div>
-      )
-    }
-    return null
-  }
-
   return (
-    <div className="mt-4 grid gap-4 lg:grid-cols-2">
-      {/* PIE CHART */}
-      <div className="flex h-[400px] flex-col items-center justify-center rounded-md border p-4">
-        <h1 className="text-lg">Progreso de tareas</h1>
-        {tasks.length > 0 ? (
-          <ResponsiveContainer width="100%" height="100%">
-            <PieChart>
-              <Pie data={projectsGroups} dataKey="value" fill="#8884d8">
-                {projectsGroups.map((_, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORSFORPROJECTS[index % COLORSFORPROJECTS.length]}
-                  />
-                ))}
-              </Pie>
-              <Tooltip content={<CustomTooltip />} />
-              <Legend />
-            </PieChart>
-          </ResponsiveContainer>
-        ) : (
-          <div className="flex h-full w-full items-center justify-center">
-            No hay tareas aún
-          </div>
-        )}
-      </div>
+    <>
+      <div className="grid gap-4 lg:grid-cols-2">
+        {/* PIE CHART */}
+        <div className="flex h-[400px] flex-col items-center justify-center rounded-md border p-4">
+          <h1 className="text-lg">Progreso de tareas</h1>
+          {tasks.length > 0 ? (
+            <ResponsiveContainer width="100%" height="100%">
+              <PieChart>
+                <Pie data={projectsGroups} dataKey="value" fill="#8884d8">
+                  {projectsGroups.map((_, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORSFORPROJECTS[index % COLORSFORPROJECTS.length]}
+                    />
+                  ))}
+                </Pie>
+                <Tooltip content={<CustomTooltip total={total} />} />
+                <Legend />
+              </PieChart>
+            </ResponsiveContainer>
+          ) : (
+            <div className="flex h-full w-full items-center justify-center">
+              No hay tareas aún
+            </div>
+          )}
+        </div>
 
-      {/* BAR CHART */}
-      <div className="flex h-[400px] flex-col items-center justify-center rounded-md border p-4">
-        <h1 className="text-lg">Distribución de horas por proyecto</h1>
-        {tasks.length > 0 ? (
-          <ResponsiveContainer width="100%" height="100%">
-            <BarChart data={projectsGroups}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              {/* <Tooltip content={<CustomTooltip />} /> */}
-              <Bar dataKey="value">
-                {projectsGroups.map((_, index) => (
-                  <Cell
-                    key={`bar-${index}`}
-                    fill={COLORSFORPROJECTS[index % COLORSFORPROJECTS.length]}
-                  />
-                ))}
-              </Bar>
-            </BarChart>
-          </ResponsiveContainer>
-        ) : (
-          <div className="flex h-full w-full items-center justify-center">
-            No hay tareas aún
-          </div>
-        )}
+        {/* BAR CHART */}
+        <div className="flex h-[400px] flex-col items-center justify-center rounded-md border p-4">
+          <h1 className="text-lg">Distribución de horas por proyecto</h1>
+          {tasks.length > 0 ? (
+            <ResponsiveContainer width="100%" height="100%">
+              <BarChart data={projectsGroups}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                {/* <Tooltip content={<CustomTooltip />} /> */}
+                <Bar dataKey="value">
+                  {projectsGroups.map((_, index) => (
+                    <Cell
+                      key={`bar-${index}`}
+                      fill={COLORSFORPROJECTS[index % COLORSFORPROJECTS.length]}
+                    />
+                  ))}
+                </Bar>
+              </BarChart>
+            </ResponsiveContainer>
+          ) : (
+            <div className="flex h-full w-full items-center justify-center">
+              No hay tareas aún
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Proyecto</th>
+            <th>Horas estimadas</th>
+            <th>Periodo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td>{task.project.name}</td>
+              <td>{task.estimatedHours}</td>
+              <td>{task.expectedCompletionDate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   )
 }
--- src/pages/WorkDistributions/WorkDistribution.tsx
+++ src/pages/WorkDistributions/WorkDistribution.tsx
@@ -1,12 +1,30 @@
 import { CustomSelect } from "@/components/ui/customSelect"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { WorkDistributionCharts } from "./Admin/components/WorkDistributionCharts"
 import { useGetUsers } from "../Users/api/useGetUsers"
 import { RoleEnum } from "../Users/types"
 import { useGetUserTasks } from "../Projects/ProjectIdPage/api/useGetUserTasks"
 import { TaskStatusEnum } from "../Tasks/types"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { CalendarIcon } from "lucide-react"
+import { Calendar } from "@/components/ui/calendar"
+import { es } from "date-fns/locale"
+import { format } from "date-fns"
+import jsPDF from "jspdf"
+import html2canvas from "html2canvas"
 
 export const WorkDistribution = () => {
+  const [startDate, setStartDate] = useState<Date>()
+  const [endDate, setEndDate] = useState<Date>()
+  const [currentMonth, setCurrentMonth] = useState(new Date())
+  const containerRef = useRef<HTMLDivElement | null>(null)
+
   const [selectedEmployee, setSelectedEmployee] = useState<string | null>(null)
 
   const { data: users = [], isLoading: isLoadingUsers } = useGetUsers()
@@ -20,9 +38,17 @@ export const WorkDistribution = () => {
     },
   })
 
-  const userTasks = (tasks || []).filter(
-    (task) => task.status === TaskStatusEnum.COMPLETED,
-  )
+  const userTasks = (tasks || [])
+    .filter((task) => task.status === TaskStatusEnum.COMPLETED)
+    .filter((task) => {
+      if (startDate && endDate) {
+        return (
+          new Date(task.expectedCompletionDate) >= startDate &&
+          new Date(task.expectedCompletionDate) <= endDate
+        )
+      }
+      return true
+    })
 
   if (isLoadingUsers || isLoadingTasks) {
     return (
@@ -32,22 +58,111 @@ export const WorkDistribution = () => {
     )
   }
 
+  const handleExportToPDF = async () => {
+    const pdf = new jsPDF("p", "pt", "letter")
+
+    if (!containerRef.current) return
+
+    const canvas = await html2canvas(containerRef.current, { scale: 2 })
+    const imgData = canvas.toDataURL("image/png")
+
+    const pageWidth = pdf.internal.pageSize.getWidth()
+    const pageHeight = pdf.internal.pageSize.getHeight()
+
+    // Ajustar la imagen al ancho de la página PDF, manteniendo proporciones
+    const imgWidth = pageWidth
+    const imgHeight = (canvas.height * pageWidth) / canvas.width
+
+    if (imgHeight > pageHeight) {
+      console.warn(
+        "El contenido excede la altura de la página. Se ajustará automáticamente.",
+      )
+    }
+
+    pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight)
+
+    const employee = employees.find(
+      (user) => user.id === Number(selectedEmployee),
+    )?.name
+
+    const reportName = `Reporte de distribución de trabajo - ${employee}.pdf`
+
+    pdf.save(reportName)
+  }
+
   return (
     <div>
       <div className="flex items-center justify-center text-xl font-semibold">
         Reporte de distribución de trabajo
       </div>
-      <div className="mt-4 max-w-sm">
-        <CustomSelect
-          options={employees.map((user) => ({
-            label: user.name,
-            value: user.id.toString(),
-          }))}
-          defaultValue={selectedEmployee || ""}
-          onValueChange={setSelectedEmployee}
-          placeholder="Seleccione un empleado"
-          showSearch
-        />
+      <div className="mt-4 flex flex-col flex-wrap items-center space-x-4 md:flex-row">
+        <div className="min-w-[300px] max-w-sm">
+          <CustomSelect
+            options={employees.map((user) => ({
+              label: user.name,
+              value: user.id.toString(),
+            }))}
+            defaultValue={selectedEmployee || ""}
+            onValueChange={setSelectedEmployee}
+            placeholder="Seleccione un empleado"
+            showSearch
+          />
+        </div>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant={"outline"}
+              className={cn(
+                "w-full max-w-sm justify-start text-left font-normal md:w-48",
+                !startDate && "text-muted-foreground",
+              )}
+            >
+              <CalendarIcon />
+              {startDate ? (
+                format(startDate, "dd-MM-yyyy", { locale: es })
+              ) : (
+                <span>Fecha de inicio</span>
+              )}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar
+              mode="single"
+              selected={startDate}
+              onSelect={setStartDate}
+              initialFocus
+              month={currentMonth}
+              onMonthChange={setCurrentMonth}
+            />
+          </PopoverContent>
+        </Popover>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant={"outline"}
+              className={cn(
+                "w-full max-w-sm justify-start text-left font-normal md:w-48",
+                !endDate && "text-muted-foreground",
+              )}
+            >
+              <CalendarIcon />
+              {endDate ? (
+                format(endDate, "dd-MM-yyyy", { locale: es })
+              ) : (
+                <span>Fecha de fin</span>
+              )}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar
+              mode="single"
+              selected={endDate}
+              onSelect={setEndDate}
+              initialFocus
+            />
+          </PopoverContent>
+        </Popover>
+        <Button onClick={handleExportToPDF}>Exportar</Button>
       </div>
       {userTasks.length === 0 ? (
         <div>
@@ -56,7 +171,9 @@ export const WorkDistribution = () => {
           </div>
         </div>
       ) : (
-        <WorkDistributionCharts tasks={userTasks} />
+        <div className="mt-4" ref={containerRef}>
+          <WorkDistributionCharts tasks={userTasks} />
+        </div>
       )}
     </div>
   )
